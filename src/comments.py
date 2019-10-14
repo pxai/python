71a0be0d7b1ed026cfe90b0e3da8d3c2110d14ac
Pello Altadill <pello.altadill@gluckgames.com>
2019-10-14 08:57:05 +0200

    More on general

--- src/comments.py
+++ src/comments.py
@@ -1,1 +1,10 @@
 # This is one line comment
+
+# next is a comment to set
+# -*- coding: utf-8 -*-
+
+"""
+This
+is a
+multiline comment
+"""
--- src/var_types.py
+++ src/var_types.py
@@ -5,3 +5,18 @@ weight = 78.54
 temperature = -4.34
 pythonRules = True
 imInmortal = False
+
+prayer = """A multiline
+string looks
+like this"""
+
+# assignment
+a = b = c = 0
+
+# but not strongly typed...
+a = True
+c = 'Hello'
+
+a = 10
+b = -a
+c = 42
