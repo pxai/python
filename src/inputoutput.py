3a83586b52568114b92b215f24b69c19aebab6e0
Pello Altadill <pello.altadill@gluckgames.com>
2019-10-16 09:16:38 +0200

    Input stuff

---
+++ src/inputoutput.py
@@ -0,0 +1,14 @@
+print("Hello world")
+a = 42
+b = 15
+
+# concatenation with , for any type
+print("a is ", a, " and b ", b)
+
+# string with +
+print(str(a) + "," + str(b))
+
+# input
+print("{0} and {1}".format(a, b))
+c = input("Enter a number: ")
+print("You wrote: ", c)
