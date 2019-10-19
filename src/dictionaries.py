9fd739f814377efbedf061ddee464b9de2ebabd3
Pello Altadill <pello.altadill@gluckgames.com>
2019-10-19 09:22:58 +0200

    Added dictionaries

---
+++ src/dictionaries.py
@@ -0,0 +1,13 @@
+ages = {'Gandalf':1432, 'Aragorn':543, 'Bilbo':112}
+emptyDictionary = {}
+
+# using the builtin function:
+trigValues = dict(sin=34, cos=353.23, tan=34)
+otherAges = dict([['Frodo',34], ['Samwise',35]])
+
+ages['Bilbo'] = 120
+ages['Radagast'] = 1423
+
+# iterating...
+for key in ages:
+	print key , " : " , ages[key]
